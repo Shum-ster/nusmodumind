fca9bce71a39a7fe17757a99658a912f856d104d
Shum Xian You <shumxianyou@gmail.com>
2026-05-24 19:54:15 +0800

    Initialise frontend and backend structure sourced from tutorial

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+
+export const metadata: Metadata = {
+  title: "CRUD",
+  description: "Excercise for CRUD application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="max-w-3xl mx-auto p-4">
+        <Navbar></Navbar>
+        <div className="mt-8">
+        {children}
+        </div>
+        </div>
+      </body>
+    </html>
+  );
+}
